acd1153a5ab0e697fc5c85237aae80f86e172746
VictorMorales94 <teraonline9494@gmail.com>
2025-12-01 14:01:35 -0500

    Implement SQL connection and data reading functions
    Added functions to connect to SQL and read data from two tables.

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,29 @@
+import pandas as pd
+import sqlalchemy
+from config import SQL_MAIN, SQL_BLINDAJE
 
+def conectar_sql(server, database):
+    conn_string = (
+        f"mssql+pyodbc://@{server}/{database}"
+        "?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+    )
+    return sqlalchemy.create_engine(conn_string)
+
+def leer_conversaciones():
+    engine = conectar_sql(SQL_MAIN["server"], SQL_MAIN["database"])
+    query = """
+    SELECT [Ticket], [Texto de interacción], [Asesor],
+           [Fecha ticket - Hora ticket], [Grupo]
+    FROM [CONVERSACIONES LARAIGO]
+    """
+    return pd.read_sql(query, engine)
+
+def leer_blindaje():
+    engine = conectar_sql(SQL_BLINDAJE["server"], SQL_BLINDAJE["database"])
+    query = """
+    SELECT TICKET, [Nivel 1], [Nivel 2], [Nivel 3], [Nivel 4], [BaseName]
+    FROM [dbo].[TICKETS_PROCESADOS]
+    """
+    df = pd.read_sql(query, engine)
+    df.rename(columns={"TICKET": "ticket"}, inplace=True)
+    return df
